b970542d83756e73c1fd2206367602cd46cc3169
Evan Sosenko <razorx@evansosenko.com>
2019-06-10 21:30:17 -0700

    Update to AVA v2

---
+++ ava.config.js
@@ -0,0 +1,13 @@
+export default () => {
+  const ext = process.env.SMOKE_TEST === 'true' ? 'test' : 'spec'
+  return {
+    files: [
+      `**/*.${ext}.js`,
+      '!dist/**/*',
+      '!package/**/*'
+    ],
+    require: [
+      '@babel/register'
+    ]
+  }
+}
