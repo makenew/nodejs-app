2ee57786e1eb9c2629be85dfe8e38029e26abee4
Evan Sosenko <razorx@evansosenko.com>
2020-11-11 13:12:40 -0800

    Do not tag all images with git hash

--- .circleci/push.sh
+++ .circleci/push.sh
@@ -5,21 +5,19 @@ set -u
 
 docker_repo=$1
 
-docker_tag="${CIRCLE_SHA1:-latest}"
 pkg_version=$(jq -r '.version' package.json)
 
 if [[ "${CIRCLE_JOB:-push}" == 'push-ci' ]]; then
-  docker_tag="ci.${docker_tag}"
+  docker_tag="ci.${CIRCLE_SHA1:-latest}"
+  docker tag $APP_NAME "${docker_repo}:${docker_tag}"
+  docker push "${docker_repo}:${docker_tag}"
+  echo
+  echo "> Pushed ${docker_repo}:${docker_tag}"
+  echo
 fi
 
 docker tag $APP_NAME "${docker_repo}:latest"
-docker tag $APP_NAME "${docker_repo}:${docker_tag}"
 docker tag $APP_NAME "${docker_repo}:${pkg_version}"
-
-docker push "${docker_repo}:${docker_tag}"
-echo
-echo "> Pushed ${docker_repo}:${docker_tag}"
-echo
 
 if [[ "$(git log -1 --pretty='%s')" == "${pkg_version}" ]]; then
   docker push "${docker_repo}:${pkg_version}"
