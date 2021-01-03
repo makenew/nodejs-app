7ec8b3ced9daebae97939985a5c5d06f52c16142
Evan Sosenko <razorx@evansosenko.com>
2021-01-03 13:00:07 -0600

    Update to mlabs-koa v10

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import boot from './boot'
 
-export { default as createServer } from '@meltwater/mlabs-koa'
+export { createServer } from '@meltwater/mlabs-koa'
 export { createDependencies } from '../lib'
 export { default as boot, loadConfig } from './boot'
 export { default as configure } from './config'
