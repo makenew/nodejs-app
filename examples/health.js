e4be9972e4995a3892e10b664c62a360b3524272
Evan Sosenko <razorx@evansosenko.com>
2021-05-11 15:19:05 -0700

    Run format

--- examples/health.js
+++ examples/health.js
@@ -2,14 +2,15 @@ import { URL } from 'url'
 
 import { httpGetJson } from '@meltwater/mlabs-koa'
 
-export default ({ log, koaApi }) => async (check = '') => {
-  const url = new URL(`${koaApi}/health/${check}`)
-  const { healthy, error } = await httpGetJson({
-    hostname: url.hostname,
-    path: url.pathname,
-    port: url.port,
-    headers: { accept: 'application/json' }
-  })
-  if (error) throw new Error(error)
-  return healthy
-}
+export default ({ log, koaApi }) =>
+  async (check = '') => {
+    const url = new URL(`${koaApi}/health/${check}`)
+    const { healthy, error } = await httpGetJson({
+      hostname: url.hostname,
+      path: url.pathname,
+      port: url.port,
+      headers: { accept: 'application/json' }
+    })
+    if (error) throw new Error(error)
+    return healthy
+  }
